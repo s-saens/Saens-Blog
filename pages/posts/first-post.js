0723cdc551b15be37f3be10a05c69bd97fc43f27
Song_Saens <ssh9199_@naver.com>
2021-01-11 16:29:58 +0900

    [#003] BASICS > Assets, Metadata, and CSS
    더 알아볼 것
    - applying TailwindCSS
    - module.css 를 쓰는 이유

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,17 @@
 import Link from 'next/link'
 import Head from 'next/head'
+
+import Layout from '../../components/layout'
+
 export default function FirstPost() {
    return (
-      <>
+      <Layout >
          <Head>
             <title> FirstPost </title>
          </Head>
-         <h1> 
+         <h1 className=""> 
             First Post
          </h1>
-
-         <Link href="/">
-            <a> go home </a>
-         </Link>
-      </>
+      </Layout>
    );
 }
